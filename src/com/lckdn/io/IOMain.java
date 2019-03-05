269f9f22032819e1b489c4a7c8c88fc8ceab155b
Alexander Sergeenko <asergeenko@neoflex.ru>
2019-03-05 14:02:31 +0300

    - BufferedReader - BufferedWriter - FileInputStream - FileOutputStream

--- src/com/lckdn/io/IOMain.java
+++ src/com/lckdn/io/IOMain.java
@@ -3,34 +3,44 @@ package com.lckdn.io;
 import com.lckdn.io.collections.AverageStudentGrade;
 import com.lckdn.io.collections.IOHelper;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashMap;
+import java.io.*;
 import java.util.Map;
 
 public class IOMain {
 
-    public static void main(String[] args) {
+    private static final String GRADE_BOOK_TXT = "GradeBook.txt";
+    private static final String GRADE_BOOK_BYTE_TXT = "GradeBookByte.txt";
+
+    public static void main(String[] args) throws IOException {
         Map<String, AverageStudentGrade> grades = IOHelper.createGrades();
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter("GradeBook.txt");
+        writeFile(grades);
+        readFile();
 
+        try (FileInputStream reader = new FileInputStream(GRADE_BOOK_TXT); FileOutputStream writer = new FileOutputStream(GRADE_BOOK_BYTE_TXT)) {
+
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.println(c);
+                writer.write(c);
+            }
+        }
+    }
+
+    private static void readFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(GRADE_BOOK_TXT));
+        String s;
+        while ((s = reader.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+
+    private static void writeFile(Map<String, AverageStudentGrade> grades) throws IOException {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(GRADE_BOOK_TXT))) {
             for (Map.Entry<String, AverageStudentGrade> entry : grades.entrySet()) {
-
                 writer.write("======================\n");
                 writer.write("Student: " + entry.getValue().getName() + "\nAverage grade: " + entry.getValue().getAverageGrade() + "\n");
             }
-            //writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            writer.flush();
         }
     }
 }
