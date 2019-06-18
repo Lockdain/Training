fb3832baeb3e7d06c3aa8a572792ef0c6a34d99c
lockdain <lockdain@gmail.com>
2019-06-18 10:45:08 +0300

    New lambda expression examples added.

--- src/com/lckdn/lambdas/medium/NumericTest.java
+++ src/com/lckdn/lambdas/medium/NumericTest.java
@@ -8,6 +8,15 @@ interface MyGreeting {
     String processName(String str);
 }
 
+interface MyString {
+    String myStringFunction(String str);
+}
+
+interface MyGeneric<T> {
+    T compute(T t);
+}
+
+
 class Test {
     public static void main(String[] args) {
         NumericTest isEven = (n) -> (n % 2) == 0;
@@ -21,5 +30,48 @@ class Test {
 
         System.out.println(morningGreeting.processName("Alex"));
         System.out.println(eveningGreeting.processName("Alex"));
+
+        // Block lambda to reverse string
+        MyString reverseStr = (str) -> {
+            String result = "";
+
+            for (int i = str.length() - 1; i >= 0; i--)
+                result += str.charAt(i);
+
+            return result;
+        };
+
+        // Output: omeD adbmaL
+        System.out.println(reverseStr.myStringFunction("Lambda Demo"));
+
+        main1();
+    }
+
+    public static void main1() {
+        // String version of MyGenericInteface
+        MyGeneric<String> reverse = (str) -> {
+            String result = "";
+
+            for (int i = str.length() - 1; i >= 0; i--)
+                result += str.charAt(i);
+
+            return result;
+        };
+
+        // Integer version of MyGeneric
+        MyGeneric<Integer> factorial = (Integer n) -> {
+            int result = 1;
+
+            for (int i = 1; i <= n; i++)
+                result = i * result;
+
+            return result;
+        };
+
+        // Output: omeD adbmaL
+        System.out.println(reverse.compute("Lambda Demo"));
+
+        // Output: 120
+        System.out.println(factorial.compute(5));
     }
 }
